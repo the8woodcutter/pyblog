9deecb05e95fdbb48eeef5b39a2d6064caf37d37
chunk <chunk@pub>
2023-04-26 03:39:15 -0600

    Started a thing

--- pb.py
+++ pb.py
@@ -3,17 +3,12 @@ import os
 import re
 
 class pb():
-	# list global variables
-	# list global pathnames
-	# list global functions
-	# initialize as a script with __name__ == __main__
-		# ask for input
-		# make html pages
-		# reset site
-		# exit
+# list global variables
+# list global functions
+# execute as script
 
-	# do we parse this?
-	global_config = ".config"
+	# do we parse this?  or include this?
+	global_config = ".config.py"
 
 
 
@@ -32,4 +27,88 @@ class pb():
 	def disqus_body():
 		pass
 
-	
+	def disqus_footer():
+		pass
+
+	def get_html_file_content():
+		pass
+
+	def edit():
+		pass
+
+	def twitter_card():
+		pass
+
+	def twitter():
+		pass
+
+	def is_boilerplate_file():
+		pass
+
+	def create_html_page():
+		pass
+
+	def parse_file():
+		pass
+
+	def write_entry():
+		pass
+
+	def all_posts():
+		pass
+
+	def all_tags():
+		pass
+
+	def rebuild_index():
+		pass
+
+	def tags_in_post():
+		pass
+
+	def posts_with_tags():
+		pass
+
+	def rebuild_tags():
+		pass
+
+	def get_post_title():
+		pass
+
+	def get_post_author():
+		pass
+
+	def list_tags():
+		pass
+
+	def list_posts():
+		pass
+
+	def make_rss():
+		pass
+
+	def create_includes():
+		pass
+
+	def delete_includes():
+		pass
+
+	def create_css():
+		pass
+
+	def rebuild_all_entries():
+		pass
+
+	def usage():
+		pass
+
+	def reset():
+		pass
+
+	def date_version_detect():
+		pass
+
+	def do_main():
+		pass
+
+	do_main()
