014862f5d9cc2ed70de6da7f9bf57b319ce50fc1
chunk <chunk@pub>
2023-05-05 21:40:59 -0600

    just need to work out the dicts for the variables and finish sorting around the functions

--- pb.py
+++ pb.py
@@ -3,70 +3,66 @@ import os
 import re
 
 class pb():
-# list global variables
-# list global functions
-# execute as script
 
-	# do we parse this?  or include this?
 	global_config = ".config.py"
-
-
+	global_config_default = {}
 
 	def global_variables_check():
+	# here we check if .config.py exists in root dir then fill in any required missing variables with defaults and set a dict() var of these variables
+	# if file does not exist create the defaults
+		# try open .config.py for reading and parse out it's syntax to dict()
+		# except use dict(global_config_default)
+		# return dict(global_variables)
 		pass
 
-	def test_markdown():
-		pass
+# -------------------------------------------------------------------------
+# HERE: future features:
 
-	def markdown():
-		pass
+	# def test_markdown():
+	# # we are not necessarily going to use markdown, if we do we will remain pass and do later
+	# 	pass
 
-	def google_analytics():
-		pass
+	# def markdown():
+	# # remains a pass
+	# 	pass
 
-	def disqus_body():
-		pass
+# -------------------------------------------------------------------------
+# HERE: optional included html for parts of the html file generator functions:
+# OPTIONAL!  
 
-	def disqus_footer():
-		pass
+	# def google_analytics():
+	# # optional includes
+	# 	pass
 
-	def get_html_file_content():
-		pass
+	# def disqus_body():
+	# # part of optional includes
+	# 	pass
 
-	def edit():
-		pass
+	# def disqus_footer():
+	# # part of optional includes
+	# 	pass
 
-	def twitter_card():
-		pass
+	# def twitter_card():
+	# # part of optional includes
+	# 	pass
 
-	def twitter():
-		pass
+	# def twitter():
+	# # part of optional includes
+	# 	pass
+
+# -------------------------------------------------------------------------
+# HERE: functions that generate html for included sections required in html rebuild functions below:
+	# these return segments of html for other functions:
 
 	def is_boilerplate_file():
 		pass
 
-	def create_html_page():
-		pass
-
-	def parse_file():
-		pass
-
-	def write_entry():
-		pass
-
-	def all_posts():
-		pass
-
-	def all_tags():
-		pass
-
-	def rebuild_index():
-		pass
-
 	def tags_in_post():
+	# an html included item
 		pass
 
 	def posts_with_tags():
+	# an html included item
 		pass
 
 	def rebuild_tags():
@@ -84,7 +80,28 @@ class pb():
 	def list_posts():
 		pass
 
+	def get_html_file_content():
+		pass
+
+# -------------------------------------------------------------------------
+# HERE: actual generation of html files:
+
 	def make_rss():
+		pass
+
+	def rebuild_index():
+	# rebuilds index
+		pass
+
+	def all_posts():
+	# rebuilds posts index
+		pass
+
+	def all_tags():
+	# rebuilds tags index
+		pass
+
+	def create_html_page():
 		pass
 
 	def create_includes():
@@ -96,19 +113,42 @@ class pb():
 	def create_css():
 		pass
 
-	def rebuild_all_entries():
-		pass
-
-	def usage():
-		pass
-
-	def reset():
-		pass
-
 	def date_version_detect():
 		pass
 
-	def do_main():
+	def rebuild_all_entries():
+	# core function
 		pass
 
-	do_main()
+	def parse_file():
+	# does sanitization checks
+		pass
+
+# -------------------------------------------------------------------------
+# HERE: the command line utility functions for this python script:
+
+	def usage():
+	# help info on command line or docstrings - include README in here
+		pass
+
+	def write_entry():
+	# a function for opening a file or allowing on commandline writing of a post
+		pass
+
+	def edit():
+	# same as write entry but only for commandline editing of post data as it is meant to be interpreted
+		pass
+
+	def reset():
+	# core function
+		pass
+
+	def do_main():
+	# executing this script as one function
+		pass
+
+# -------------------------------------------------------------------------
+# HERE: run this script:
+	# FIRE IN THE HOLE!
+# if __name__ == __main__:
+	# this jazz...
